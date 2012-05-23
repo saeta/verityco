8c663bc074b7804da1983b2440907c770c28de7e
saeta <saeta@cs.stanford.edu>
2012-05-22 19:55:40 -0700

    Attempts at getting java.* instrumented... this could get really messy. :-(

--- src/main/java/verityco/agent/ClassTransformer.java
+++ src/main/java/verityco/agent/ClassTransformer.java
@@ -1,5 +1,6 @@
 package verityco.agent;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -11,16 +12,29 @@ import verityco.VClassVisitor;
 
 public class ClassTransformer implements ClassFileTransformer {
 
-	@Override
-	public byte[] transform(ClassLoader loader, String className,
-			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
-			byte[] classfileBuffer) throws IllegalClassFormatException {
-		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-		VClassVisitor vcv = new VClassVisitor(cw);
+  @Override
+  public byte[] transform(ClassLoader loader, String className,
+      Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+      byte[] classfileBuffer) throws IllegalClassFormatException {
+    if (className.startsWith("verityco")) {
+      return classfileBuffer;
+    }
+    ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+    System.out.println("Instrumenting class: " + className);
+    VClassVisitor vcv = new VClassVisitor(cw);
 
-		ClassReader cr = new ClassReader(classfileBuffer);
-		cr.accept(vcv, ClassReader.SKIP_FRAMES);
-		return cw.toByteArray();
-	}
+    ClassReader cr = new ClassReader(classfileBuffer);
+    cr.accept(vcv, ClassReader.SKIP_FRAMES);
+    return cw.toByteArray();
+  }
 
+  public byte[] transformClass(String className) throws IOException {
+    ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+    VClassVisitor vcv = new VClassVisitor(cw);
+
+    ClassReader cr = new ClassReader(getClass().getResourceAsStream(
+        "/" + className.replace('.', '/') + ".class"));
+    cr.accept(vcv, ClassReader.SKIP_FRAMES);
+    return cw.toByteArray();
+  }
 }
