2e61a399545d09601b49662e50737744c77cba1c
saeta <saeta@cs.stanford.edu>
2012-05-27 14:15:48 -0700

    Removing tinkercruft.

--- src/main/java/verityco/agent/ClassProcessor.java
+++ src/main/java/verityco/agent/ClassProcessor.java
@@ -1,44 +1,11 @@
 package verityco.agent;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.instrument.UnmodifiableClassException;
 
 public class ClassProcessor {
 
   public static void premain(String agentArgs, Instrumentation inst) {
-    @SuppressWarnings("rawtypes")
-    Class hashmapClass = null;
-    for (int i = 0; i < inst.getAllLoadedClasses().length; i++) {
-      String name = inst.getAllLoadedClasses()[i].getName();
-      //if (name.startsWith("java.util") || name.startsWith("java.lang")) {
-      	if (name.startsWith("java.io")) {
-      	  System.out.println(name + " is modifiable "
-            + inst.isModifiableClass(inst.getAllLoadedClasses()[i]));
-          if (name.equals("java.io.StringReader")) {
-            hashmapClass = inst.getAllLoadedClasses()[i];
-          }
-      }
-    }
     ClassTransformer ct = new ClassTransformer();
     inst.addTransformer(ct, true);
-    try {
-      // byte[] hashClassBytes = ct.transformClass("java.util.HashMap");
-      // inst.redefineClasses(new ClassDefinition(hashmapClass,
-      // hashClassBytes));
-      inst.retransformClasses(hashmapClass);
-    } catch (UnmodifiableClassException e) {
-      System.out.println(":-( SAD FACE!!!!!!!!!!!!!!");
-      // e.printStackTrace();
-      // } catch (IOException e) {
-      // System.out.println(":-(( SAD FACE!!!!!!!!!!!!!!");
-      // e.printStackTrace();
-      // } catch (ClassNotFoundException e) {
-      // System.out.println(":-((( SAD FACE!!!!!!!!!!!!!!");
-      e.printStackTrace();
-    }
-  }
-
-  public static void main(String[] args) {
-    System.out.println("In main method, when we should be in premain!");
   }
 }
--- src/main/java/verityco/agent/ClassTransformer.java
+++ src/main/java/verityco/agent/ClassTransformer.java
@@ -1,6 +1,5 @@
 package verityco.agent;
 
-import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -12,13 +11,23 @@ import verityco.VClassVisitor;
 
 public class ClassTransformer implements ClassFileTransformer {
 
+  /**
+   * Entry point into the class file transformer.
+   * 
+   * Chooses to either not transform the class (by returning the classfileBuffer
+   * or transforms the class by setting up the ASM instrumentation chain, and
+   * returning those results.
+   */
   @Override
   public byte[] transform(ClassLoader loader, String className,
       Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
       byte[] classfileBuffer) throws IllegalClassFormatException {
-    if (className.startsWith("verityco")) {
+    // Ignore ourselves.
+    if (className.startsWith("verityco/") || className.startsWith("java/")) {
       return classfileBuffer;
     }
+
+    // Transform the class
     ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
     System.out.println("Instrumenting class: " + className);
     VClassVisitor vcv = new VClassVisitor(cw);
@@ -28,13 +37,4 @@ public class ClassTransformer implements ClassFileTransformer {
     return cw.toByteArray();
   }
 
-  public byte[] transformClass(String className) throws IOException {
-    ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-    VClassVisitor vcv = new VClassVisitor(cw);
-
-    ClassReader cr = new ClassReader(getClass().getResourceAsStream(
-        "/" + className.replace('.', '/') + ".class"));
-    cr.accept(vcv, ClassReader.SKIP_FRAMES);
-    return cw.toByteArray();
-  }
 }
