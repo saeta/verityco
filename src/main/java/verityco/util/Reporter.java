d42fabc8fa7a5ce5a7335496400ec2b0e9f92709
saeta <saeta@cs.stanford.edu>
2012-05-27 14:58:34 -0700

    Changing Reporter for easier mocking.

--- src/main/java/verityco/util/Reporter.java
+++ src/main/java/verityco/util/Reporter.java
@@ -1,26 +1,11 @@
 package verityco.util;
 
-import java.io.PrintStream;
-
 public class Reporter {
 
-  public static Reporter report = new Reporter(null);
-
-  final private PrintStream ps;
-
-  public Reporter(PrintStream testPs) {
-    ps = testPs;
-  }
+  public static Reporter report = new Reporter();
 
   public void report(String error) {
-    if (ps != null) {
-      ps.println(error);
-    }
     System.out.println(error);
-  }
-
-  public PrintStream getPrintStream() {
-    return ps;
   }
 
 }
